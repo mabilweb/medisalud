e0f356dff453d16a74b865614d22dc32eb4d8c40
mabilweb <h3mabil@gmail.com>
2022-01-10 13:33:16 -0600

    medisalud

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app =express();
+const path =require('path');
+
+//settings
+app.set('port',4000);
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+//middleawres
+
+
+///routes
+app.use(require('./routes/index'));
+
+
+///static files
+app.use(express.static(__dirname + '/public'));
+
+
+///listeig the server
+app.listen(app.get('port'), () =>{
+    console.log('sirviendo  en :', app.get('port'));
+});
